e61ef7d1c4776accb53afbaa4ce5d2f3dd7bcbe6
Jeff <gggg122142yahoo@gmail.com>
2019-11-18 10:23:11 +0800

    2019/11/18 in mac

--- Demo/pages/views.py
+++ Demo/pages/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
+from products.models  import Product
+
 
 # Create your views here.
 def home_view(request,*args,**kwargs):
-    return render(request,"home.html",{})
+    product_list = Product.objects.all()
+    return render(request,"home.html",{'post_list':product_list})
     # return HttpResponse("<h1>Hello World Test</h1>")
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+def contact_view(request,*args, **kwargs):
+    return render(request,"base.html",{})
+    
